703e443d80e3d29a5d169d4250fb100ac8c08806
JaytiSimform <102812470+JaytiSimform@users.noreply.github.com>
2022-05-05 17:24:41 +0530

    HOP-889 : [Manage Negative amount ]
    Have fixed Issue of Negative Balance while Fund Management

--- src/main/java/pages/Loginpage.java
+++ src/main/java/pages/Loginpage.java
@@ -1,19 +1,19 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
-import userdata.Payinvoicedata;
+import org.openqa.selenium.*;
+import userdata.*;
 import utility.*;
 
 public class Loginpage {
 	private WebDriver driver;
 	private Registrationpage registrationpage;
 	Payinvoicedata payData = new Payinvoicedata();
+	Logindata logData;
 
 	public Loginpage(WebDriver driver) {
 		this.driver = driver;
 		registrationpage = new Registrationpage(driver);
+		logData = new Logindata();
 	}
 
 	public boolean isAttemptTextDisplayed() {
@@ -23,8 +23,9 @@ public class Loginpage {
 
 	public void doLogin(String email, String pass, String login) {
 		registrationpage.enterEmailAddress(email);
-		payData.setEmail(email);
+		//logData.setEmail(email);
 		registrationpage.enterPassword(pass);
+		//logData.setPassword(pass);
 		registrationpage.clickOnButton(login);
 	}
 
@@ -35,7 +36,10 @@ public class Loginpage {
 		} catch (Exception e) {
 			registrationpage.clickOnLink("Forgot your password?");
 			driver.navigate().back();
-			doLogin(payData.getEmail(), "Password1!", "Log in");
+			//String email = logData.getEmail();
+			//String pass = logData.getPassword();
+			//doLogin(email, pass, "Log in");
+			doLogin("hopsmokeautomation2@mailinator.com", "Password1!", "Log in");
 		}
 		return Eventhelper.waitUntilElementInvisible(driver, loginBtn);
 	}
--- src/test/java/steps/AddFundsteps.java
+++ src/test/java/steps/AddFundsteps.java
@@ -1,15 +1,11 @@
 package steps;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import pages.AddFundspage;
-import pages.Registrationpage;
-import userdata.Fundsdata;
-import utility.Driverhelper;
-import utility.Eventhelper;
-import utility.Log;
+import static org.junit.Assert.*;
+import io.cucumber.java.en.*;
+import pages.*;
+import userdata.*;
+import utility.*;
+
 
 public class AddFundsteps {
 	AddFundspage addFunds = new AddFundspage(Driverhelper.getDriver());
@@ -18,51 +14,59 @@ public class AddFundsteps {
 
 	@Then("the {string} button should be enabled | disabled")
 	public void the_button_should_be_enabled(String btnName) {
-		assertTrue(btnName +  " Button is not Enable" , addFunds.isButtonEnabled(btnName));
+		assertTrue(btnName + " Button is not Enable", addFunds.isButtonEnabled(btnName));
 	}
-	
+
 	@Then("User should save Default amount")
 	public void user_should_save_default_amount() {
 		fundData.setAmountofhopscotchBalance(addFunds.hopscotchBalanceBeforeAddingFund());
 	}
-	
+
 	@Then("the {string} button should be {string}")
 	public void the_button_should_be(String btnName, String isStatus) {
-	  boolean buttonStatus = addFunds.isButtonEnabled(btnName);
-	  if(isStatus.contains("enabled")){
-		  assertTrue(btnName +  " Button is not Enable" , buttonStatus);
-	  }
-	  else {
-		  assertTrue(btnName +  " Button is Enable" , !buttonStatus);
-	  }
+		boolean buttonStatus = addFunds.isButtonEnabled(btnName);
+		if (isStatus.contains("enabled")) {
+			assertTrue(btnName + " Button is not Enable", buttonStatus);
+		} else {
+			assertTrue(btnName + " Button is Enable", !buttonStatus);
+		}
 	}
-	
+
 	@Then("User should see {string} model open")
 	public void user_should_see_model_open(String string) {
 		boolean amountTextDisplay = addFunds.modalHeader(string) != null;
-		assertTrue(amountTextDisplay);    
+		assertTrue(amountTextDisplay);
 	}
 
-	@When("User enter {int} in amount field")
-	public void user_enter_in_amount_field1(int amount) {
-		addFunds.addAmount(amount);
-		Log.info("User is on enter Amount field value is " + amount);
+	@When("User enter {int} in amount field to {string} funds")
+	public void user_enter_in_amount_field1(int amount, String fundProcess) {
+		float amountofHopscotchBalance;
+		if (fundProcess.equals("Add")) {
+			amountofHopscotchBalance = amount + Eventhelper.numberFormat(fundData.getAmountofhopscotchBalance());
+		} else {
+			amountofHopscotchBalance = Eventhelper.numberFormat(fundData.getAmountofhopscotchBalance()) - amount;
+		}
+
+		addFunds.addAmount(amountofHopscotchBalance);
+		fundData.setAmountofhopscotchBalance(amountofHopscotchBalance);
+		Log.info("User is on enter Amount field value is " + fundData.getAmountofhopscotchBalance());
 	}
-	
+
 	@Then("User should see {string} amount on the screen")
 	public void user_should_see_amount_on_the_screen(String expectedAmountTobeChanged) {
 		Eventhelper.threadWait(3000);
-		float totalExpectedAmountBalance = fundData.getAmountofhopscotchBalance() + Float.parseFloat(expectedAmountTobeChanged);
+		Log.info("User is on enter Amount field value is " + fundData.getAmountofhopscotchBalance());
+		float totalExpectedAmountBalance = fundData.getAmountofhopscotchBalance()
+				+ fundData.getAmountofhopscotchBalance() - Float.parseFloat(expectedAmountTobeChanged);
 		assertEquals(totalExpectedAmountBalance, addFunds.hopscotchBalanceAfterAddingFund(), 0);
 	}
-	
+
 	@Then("User should see {string} amount on the screen for withdraw")
 	public void user_should_see_amount_on_the_screen_for_withdraw(String string) {
 		Eventhelper.threadWait(3000);
-		float totalExpectedAmountBalance = fundData.getAmountofhopscotchBalance() - Float.parseFloat(string);
-		assertEquals(totalExpectedAmountBalance, addFunds.hopscotchBalanceAfterAddingFund(), 1);
+		assertEquals(Float.parseFloat(string), addFunds.hopscotchBalanceAfterAddingFund(), 1);
 	}
-	
+
 	@When("User click on Close Icon")
 	public void user_click_on_close_icon() {
 		addFunds.clickonCloseIcon();
