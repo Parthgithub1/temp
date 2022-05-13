d63011f2c4b9e0db2a247e0e8a3d2ec335f75490
Yuvraj Rajput <yuvraj.r@simformsolutions.com>
2022-05-13 18:33:45 +0530

    Update Transfer button name and update balance method

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -1,22 +1,23 @@
 package pages;
 
+import java.text.DecimalFormat;
+
 import org.openqa.selenium.*;
 
 import utility.Eventhelper;
 
 public class Homepage {
 	private WebDriver driver;
-	
+
 	public Homepage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	public float hopscotchBalanceBeforeAddingFund() {
-		Eventhelper.threadWait(2000);
-		String amountofHBalance = Eventhelper.getTextofElement(driver, By.xpath("//*[contains(@class,'Balance_balance__amount')]"));
-		String newamountogHBalance = amountofHBalance.replace("$","");
-		float amountBalance = Float.parseFloat(newamountogHBalance);
-		return amountBalance;
-	}
 
+	public float hopscotchBalanceBeforeAddingFund() {
+		Eventhelper.threadWait(5000);
+		DecimalFormat df = new DecimalFormat(".00");
+		return Float.valueOf(df.format(Float.parseFloat(Eventhelper.getValueOfAttribute(driver,
+				By.xpath("//div[contains(.,'Hopscotch Balance')]/following-sibling::div[@id='HopscotchBalance']"),
+				"zurobalance-amount").replace("$", ""))));
+	}
 }
--- src/main/java/pages/Verificationpage.java
+++ src/main/java/pages/Verificationpage.java
@@ -14,7 +14,7 @@ public class Verificationpage {
 	private By ddBusinessTaxType = By.xpath("//div[@id='taxId']");
 	private By ddBusinessSelectTaxYype = By.xpath("//div[text()='SSN']");
 	private By txtBusinessTaxID = By.xpath("//input[@id='idNumber']");
-	private By btnAddBeneficial = By.xpath("//span[normalize-space()='Add beneficial owner']");
+	private By btnAddBeneficial = By.xpath("//span[normalize-space()='Add and connect instantly']");
 	private By txtAddBeneficialFirstName = By.xpath("//input[@name='ownerFirstName']");
 	private By txtAddBeneficialLastName = By.xpath("//input[@name='ownerLastName']");
 	private By txtAddBeneficialBirthDate = By.xpath("//label[@for='birthday']/following-sibling::div//input");
@@ -23,7 +23,7 @@ public class Verificationpage {
 			.xpath("//label[normalize-space()='Same as legal business address']");
 	private By lstIsBeneficialAdded = By.xpath("//p[contains(@class,'BeneficialOwner_name')]");
 	private By verifiedIcon = By.cssSelector(".VerificationStar_active__2Y_Gu");
-	private By btnAddBankDetails = By.xpath("//span[normalize-space()='Add and securely connect instantly']");
+	private By btnAddBankDetails = By.xpath("//span[normalize-space()='Add and connect instantly']");
 	private By frmIframe = By.xpath("//iframe");
 	private By btnAddBankContinue = By.xpath("//*[@id=\"aut-button\"]");
 	private By lstChase = By.xpath("(//*[text()='Chase'])");
