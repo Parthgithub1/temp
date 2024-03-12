f00e39af528bb0a6797980e9320fca4ab2d59154
Parthgithub1 <102675017+Parthgithub1@users.noreply.github.com>
2024-03-12 16:42:45 +0530

    Update Driverhelper.java
    added tlDriver.set(new ChromeDriver(options));

--- src/main/java/utility/Driverhelper.java
+++ src/main/java/utility/Driverhelper.java
@@ -47,7 +47,7 @@ public class Driverhelper {
 			WebDriverManager.chromedriver().clearDriverCache().setup();
 			WebDriverManager.chromedriver().clearResolutionCache().setup();
 			WebDriverManager.chromedriver().setup();
-			
+			tlDriver.set(new ChromeDriver(options));
 			break;
 		case "firefox":
 			FirefoxOptions foptions = new FirefoxOptions();
